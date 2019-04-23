b2c8e2dff0c53b193e6662c05747ae1b360c2c27
Pierre Gimalac <pierre.gimalac@gmail.com>
2019-04-23 12:53:29 +0200

    change commands, add print

--- src/commands.c
+++ src/commands.c
@@ -2,8 +2,17 @@
 #include <string.h>
 #include <strings.h>
 #include <network.h>
+#include <arpa/inet.h>
 
+#include "types.h"
 #include "commands.h"
+
+static const char *usages[] = {
+    "add <addr> <port>",
+    "name <name>",
+    "print",
+    "quit"
+};
 
 static void add(char *buffer){
     int rc;
@@ -11,7 +20,7 @@ static void add(char *buffer){
     name = strtok(0, " ");
     service = strtok(0, " \n");
     if (!name || !service) {
-        fprintf(stderr, "usage: add <addr> <port>\n");
+        fprintf(stderr, "Usage: %s\n", usages[0]);
         return;
     }
 
@@ -33,19 +42,64 @@ static void name(char *buffer){
     }
 }
 
-static void quit(){
+static void __print(const neighbour_t *n){
+    if (n != NULL && n->addr != NULL){
+        char ipstr[INET6_ADDRSTRLEN];
+        if (inet_ntop(AF_INET6, &n->addr->sin6_addr, ipstr, INET6_ADDRSTRLEN) != NULL){
+            printf("    @ %s / %d\n", ipstr, htons(n->addr->sin6_port));
+            return;
+        }
+    }
+    printf("    Could not display.\n");
+}
+
+static void print(char *buffer){
+    if (hashset_isempty(neighbours)){
+        printf("You have no neighbour\n");
+    } else {
+        printf("You have %d neighbour%s:\n", neighbours->size, neighbours->size == 1 ? "" : "s");
+        hashset_iter(neighbours, __print);
+    }
+    if (hashset_isempty(potential_neighbours)){
+        printf("You have no potential_neighbour.\n");
+    } else {
+        printf("You have %d potential neighbour%s:\n", potential_neighbours->size,
+                    potential_neighbours->size == 1 ? "" : "s");
+        hashset_iter(potential_neighbours, __print);
+    }
+    printf("\n");
+}
+
+static void quit(char *buffer){
     printf("Bye.\n");
     exit(0);
 }
 
+static void unknown(char *buffer){
+    printf("Usage:\n");
+    for (const char **usage = usages; *usage; usage++)
+        printf("    %s\n", *usage);
+}
+
+static const char *names[] = {
+    "add", "name", "print", "quit", NULL
+};
+
+static void (*commands[])(char*) = {
+    add, name, print, quit, NULL
+};
+
+
 void handle_command(char *buffer) {
     char *ins = strtok(buffer, " \n");
-    if (strcasecmp(ins, "add") == 0)
-        add(buffer);
-    else if (strcasecmp(ins, "name") == 0)
-        name(buffer);
-    else if (strcasecmp(ins, "quit") == 0 || strcasecmp(ins, "exit") == 0)
-        quit();
-    else
-        printf("Possible commands :\nadd <addr> <port>\nname <nickname>\nexit\nquit\n\n");
+    int ind;
+    if (ins != NULL)
+        for (ind = 0; names[ind] != NULL; ind ++)
+            if (strcasecmp(ins, names[ind]) == 0){
+                commands[ind](buffer);
+                break;
+            }
+
+    if (ins == NULL || names[ind] == NULL)
+        unknown(buffer);
 }
